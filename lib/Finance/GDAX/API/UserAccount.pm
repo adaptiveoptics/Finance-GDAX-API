3b750ec3cc2ae2480b6eae80d8e10b99caf906f6
Mark Rushing <mark@orbislumen.net>
2017-06-23 13:05:08 -0700

    Implemented User Account API

---
+++ lib/Finance/GDAX/API/UserAccount.pm
@@ -0,0 +1,62 @@
+package Finance::GDAX::API::UserAccount;
+use v5.20;
+use warnings;
+use Moose;
+use Finance::GDAX::API::Request;
+use namespace::autoclean;
+
+extends 'Finance::GDAX::API::Request';
+
+sub trailing_volume {
+    my $self = shift;
+    $self->method('GET');
+    $self->path('/users/self/trailing-volume');
+    return $self->send;
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+=head1 NAME
+
+Finance::GDAX::API::UserAccount - Account Info
+
+=head1 SYNOPSIS
+
+  use Finance::GDAX::API::UserAccount;
+
+  $account = Finance::GDAX::API::UserAccount->new;
+
+  # List of trailing volume
+  $trailing = $account->trailing_volume;
+
+=head2 DESCRIPTION
+
+Returns a list of hashes, representing the Trailing Volume on the account.
+
+=head1 METHODS
+
+=head2 C<trailing_volume>
+
+From the GDAX API:
+
+This request will return your 30-day trailing volume for all
+products. This is a cached value that’s calculated every day at
+midnight UTC.
+
+  [
+    {
+        "product_id": "BTC-USD",
+        "exchange_volume": "11800.00000000",
+        "volume": "100.00000000",
+        "recorded_at": "1973-11-29T00:05:01.123456Z"
+    },
+    {
+        "product_id": "LTC-USD",
+        "exchange_volume": "51010.04100000",
+        "volume": "2010.04100000",
+        "recorded_at": "1973-11-29T00:05:02.123456Z"
+    }
+  ]
+
+=cut
