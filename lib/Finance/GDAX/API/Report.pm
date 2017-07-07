1b954f019e4c29c0ac9a35fffe90a9da85e3c431
Mark Rushing <mark@orbislumen.net>
2017-07-06 21:23:13 -0700

    Removed leftover warn that was in the get method for debuggind

--- lib/Finance/GDAX/API/Report.pm
+++ lib/Finance/GDAX/API/Report.pm
@@ -43,7 +43,6 @@ sub get {
     die 'no report_id specified to get' unless $report_id;
     $self->report_id($report_id);
     my $path = "/reports/$report_id";
-    warn $path;
     $self->path($path);
     $self->method('GET');
     return $self->send;
